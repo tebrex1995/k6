005dde38e92c0c68d2b686988df686e17d5979d3
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-12-03 18:48:20 +0100

    added structure

--- first-script.js
+++ first-script.js
@@ -1,12 +1,20 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import exec from 'k6/execution';
 
 export const options = {
   vus: 1,
   duration: '30s',
 };
 
+export function setup() {
+  let res = http.get('https://test.k6.local/status');
+  if (res.error) {
+    exec.test.abort('Aborting test. Application is down!');
+  }
+}
+
 export default function () {
-  http.get('https://test.k6.io');
+  http.get('https://test.k6.local/some-page');
   sleep(1);
 }
